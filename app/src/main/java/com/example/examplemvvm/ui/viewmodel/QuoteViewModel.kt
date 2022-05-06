9a4de5ce0531be8da1ccc4fd71a6f4b617b5cafd
enmanuel52 <enmanuel52@gmail.com>
2022-05-06 15:33:19 -0400

    with dagger hilt full

--- app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/ui/viewmodel/QuoteViewModel.kt
@@ -7,15 +7,18 @@ import com.example.examplemvvm.data.model.QuoteModel
 import com.example.examplemvvm.data.model.QuoteProvider
 import com.example.examplemvvm.domain.GetQuotesUseCase
 import com.example.examplemvvm.domain.GetRandomQuotesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class QuoteViewModel: ViewModel() {
+@HiltViewModel
+class QuoteViewModel @Inject constructor(
+    private val getQuotesUseCase: GetQuotesUseCase,
+    private val getRandomQuotesUseCase: GetRandomQuotesUseCase,
+) : ViewModel() {
     //encapsulo el modelo en un livedata
     val quoteModel = MutableLiveData<QuoteModel>()
     val isLoading = MutableLiveData<Boolean>()
-
-    var getQuotesUseCase = GetQuotesUseCase()
-    var getRandomQuotesUseCase = GetRandomQuotesUseCase()
 
     fun onCreate() {
         viewModelScope.launch {
